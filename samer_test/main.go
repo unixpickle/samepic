e187233c12d0e63aee1be2d2818077b5cc2fa9fe
Alex Nichol <unixpickle@gmail.com>
2016-08-17 19:55:27 -0400

    created & tested average perceptual hash
    On my dataset, it got 100% accuracy on negatives and ~50% accuracy on positives, which is pretty decent.

---
+++ samer_test/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/unixpickle/samepic"
+)
+
+const DefaultSampleCount = 100
+
+func main() {
+	if len(os.Args) != 3 && len(os.Args) != 4 {
+		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "samer samples [count]")
+		fmt.Fprintln(os.Stderr, "Available samers:")
+		fmt.Fprintln(os.Stderr, " - avghash")
+		os.Exit(1)
+	}
+
+	count := DefaultSampleCount
+	if len(os.Args) == 4 {
+		var err error
+		count, err = strconv.Atoi(os.Args[3])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Invalid count:", os.Args[3])
+			os.Exit(1)
+		}
+	}
+
+	var samer samepic.Samer
+	switch os.Args[1] {
+	case "avghash":
+		samer = &samepic.AverageHash{}
+	default:
+		fmt.Fprintln(os.Stderr, "Unknown samer:", os.Args[1])
+		os.Exit(1)
+	}
+	samples, err := samepic.NewDirSamples(os.Args[2])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to read sample dir:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Rating...")
+	pos, neg, err := samepic.Rate(samer, samples, samepic.DefaultManipulator, count)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to rate:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Positive rating:", pos)
+	fmt.Println("Negative rating:", neg)
+}
